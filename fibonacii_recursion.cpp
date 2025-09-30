c6d33d500f2e3fc0c86527fbcf538b20db40e780
shantanu <yadavshantanu9454@gmail.com>
2025-09-30 12:41:38 +0530

    fibonacii_recursion.cpp

--- fibonacii_recursion.cpp
+++ fibonacii_recursion.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 // print fibonacci series using recursion
 void fibonacci(int a, int b,int n){
-    if(n==0)return;
-    cout<< a+b;
-    fibonacci(b,a+b,n-1);
+    if(n==0) return ;
+    int c=a+b;
+    cout<<c<<" ";
+    fibonacci(b,c,n-1);
 }
+
 int main(){
     int n;
     cout<<"enter the number of terms in fibonacci series ";
     cin>>n ;
-    fibonacci(0,1,n);
+    if(n>=2){
+        cout<<0<<" "<<1<<" "; 
+    }
+    
+    fibonacci(0,1,n-2);
     return 0;
 }
